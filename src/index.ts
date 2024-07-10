4fb10814872282ff0cb98885b5d96dcf4fb31add
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-07-10 17:40:57 +0300

    добавлена логика открытия попапа, выведения в консоль при сабмите данных формы, 30 минут

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,56 @@
-const enum Num {
-  FIRST = "first",
-  SECOND = "second",
-}
+const documentHandler = (event: MouseEvent) => {
+  const target = event.target as Node;
+  const popup = document.querySelector(".popup");
+  if (popup && target === popup) {
+    popup.classList.remove("display");
+    document.removeEventListener("click", documentHandler);
+  }
+};
 
-console.log("Hello World");
-console.log(Num.FIRST);
+const mainButton = document.querySelector(".mainButton");
+
+mainButton?.addEventListener("click", (event) => {
+  event.stopPropagation();
+  const popup = document.querySelector(".popup");
+
+  if (popup) {
+    popup.classList.add("display");
+
+    document.addEventListener("click", documentHandler);
+  }
+});
+
+const closePopup = () => {
+  const popup = document.querySelector(".popup");
+  popup?.classList.remove("display");
+};
+
+const closeButton = document.querySelector(".popup-closeButton");
+closeButton?.addEventListener("click", () => {
+  closePopup();
+});
+
+const form: HTMLFormElement | null = document.querySelector(".popup-form");
+form?.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const contact = formData.get("contact");
+  const password = formData.get("password");
+  const isRemember = formData.get("isRemember") === "on" ? true : false;
+  console.log({
+    contact,
+    password,
+    isRemember,
+  });
+  closePopup();
+});
+
+const backUpButton = document.querySelector(".popup-textButton");
+backUpButton?.addEventListener("click", () =>
+  console.log("Перейти в форму для восстановления акаунта")
+);
+
+const regButton = document.querySelector(".popup-regButton");
+regButton?.addEventListener("click", () =>
+  console.log("Перейти в форму для регистрации акаунта")
+);
